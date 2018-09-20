7410dc3225035207a9404166425e26e3c651bd11
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-20 15:39:07 -0700

    add an initialState and set it as the default value of the state parameter in rootReducer

--- basics.js
+++ basics.js
@@ -1,9 +1,15 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
-const rootReducer = (state, action) => {
+const initialState = {
+  counter: 0
+}
+
+// Reducer using default value ES6
+const rootReducer = (state=initialState, action) => {
   return state
 }
 
+// Store
 const store = createStore(rootReducer)
 console.log(store.getState()) 
