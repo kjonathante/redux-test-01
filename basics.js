2aba8deb046dd0848dd53f32816c27a6b6da9a3f
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-20 15:48:51 -0700

    create dispatches and actions

--- basics.js
+++ basics.js
@@ -7,9 +7,26 @@ const initialState = {
 
 // Reducer using default value ES6
 const rootReducer = (state=initialState, action) => {
+  switch( action.type ) {
+    case 'INC_COUNTER':
+      return {
+        ...state,
+        counter: state.counter+1
+      }
+      case 'ADD_COUNTER':
+      return {
+        ...state,
+        counter: state.counter+action.value
+      }
+  }
   return state
 }
 
 // Store
 const store = createStore(rootReducer)
 console.log(store.getState()) 
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 10})
+console.log(store.getState())
