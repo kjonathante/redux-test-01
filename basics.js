f439656d41495275f4229ffb2cd9f5fb7fa024e1
Kit Jonathan Te <kjonathante@gmail.com>
2018-09-20 15:34:53 -0700

    We are in node.js, using require not import. Create rootReducer and the store.

---
+++ basics.js
@@ -0,0 +1,9 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const rootReducer = (state, action) => {
+  return state
+}
+
+const store = createStore(rootReducer)
+console.log(store.getState()) 
